05a59ea3178b432c703cced81c4f3ea939641939
Weslley Pereira Alberto <weslleyalberto@gmail.com>
2021-11-13 17:40:47 -0300

    Adicionado Carrinho controller

---
+++ src/NerdStore.WebApps.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApps.MVC.Controllers
+{
+    
+
+    public class CarrinhoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        private readonly IMediatrHandler _mediatrHandler;
+
+        public CarrinhoController(IProdutoAppService produtoAppService, 
+            IMediatrHandler mediatrHandler)
+        {
+            _produtoAppService = produtoAppService;
+            _mediatrHandler = mediatrHandler;   
+        }
+        public IActionResult Index() => View();
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id , int quantidade)
+        {
+            var produto  = await _produtoAppService.ObterPorId(id);
+            if (produto is null) return BadRequest();
+
+            if(produto.QuantidadeEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insufucuente";
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+            var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+            await _mediatrHandler.EnviarComando(command);
+            //Se tudo deu certo?
+
+            TempData["Erro"] = "Produto Indisponível";
+            return RedirectToAction("ProdutoDetalhe","Vitrine",new {id});
+        }
+    }
+}
